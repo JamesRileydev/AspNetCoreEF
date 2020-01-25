4552802f7177cf63d6239c61e2d6dbd226bdf945
JamesRileydev <jamesriley.dev@gmail.com>
2020-01-25 08:39:43 -0500

    Added AppController and view

---
+++ ArtMarketplace/Controllers/AppController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ArtMarketplace.Controllers
+{
+    public class AppController: Controller
+    {
+        public AppController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            throw new InvalidOperationException();
+            return View();
+        }
+    }
+}
--- ArtMarketplace/Startup.cs
+++ ArtMarketplace/Startup.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ArtMarketplace
 {
@@ -11,14 +11,28 @@ namespace ArtMarketplace
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //Add error page
+            }
+
             app.UseStaticFiles();
             app.UseNodeModules();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg => cfg.MapControllerRoute("Fallback", "{controller}/{action}/{id?}",
+                new { controller = "App", action = "Index" }));
         }
     }
 }
