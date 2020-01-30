a005ebbf7bb8955305ff2bf210cc3b9b15a553e2
JamesRileydev <jamesriley.dev@gmail.com>
2020-01-29 21:56:26 -0500

    Added NullMailService

--- ArtMarketplace/Controllers/AppController.cs
+++ ArtMarketplace/Controllers/AppController.cs
@@ -1,10 +1,18 @@
-﻿using ArtMarketplace.ViewModels;
+﻿using ArtMarketplace.Services;
+using ArtMarketplace.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ArtMarketplace.Controllers
 {
     public class AppController : Controller
     {
+        private NullMailService _mailService;
+
+        public AppController(NullMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -23,6 +31,7 @@ namespace ArtMarketplace.Controllers
              if (ModelState.IsValid)
             {
                 //Send the email
+                _mailService.SendMail("jamesriley.dev@gmail.com", vm.Subject, $"From: {vm.Name} - {vm.Email}, Message: {vm.Message}");
 
             }
             else
---
+++ ArtMarketplace/Services/NullMailService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ArtMarketplace.Services
+{
+    public class NullMailService
+    {
+        private readonly ILogger<NullMailService> _logger;
+
+        public NullMailService(ILogger<NullMailService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void SendMail(string to, string subject, string body)
+        {
+            _logger.LogInformation($"To: {to} Subject: {subject} Body: {body}");
+        }
+    }
+}
