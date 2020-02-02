812fca1175978e5147540c2db662bcda73380b24
JamesRileydev <jamesriley.dev@gmail.com>
2020-02-02 17:22:16 -0500

    Corrected database connection string name

--- ArtMarketplace/Data/Contexts/ArtContext.cs
+++ ArtMarketplace/Data/Contexts/ArtContext.cs
@@ -5,6 +5,9 @@ namespace ArtMarketplace.Data.Contexts
 {
     public class ArtContext : DbContext
     {
+        public ArtContext(DbContextOptions<ArtContext> options): base(options)
+        { }
+
         public DbSet<Product> Products{ get; set; }
         public DbSet<Order> Orders { get; set; }
     }
