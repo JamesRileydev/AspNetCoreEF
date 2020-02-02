3371ca81b4d91a98546d24f1a29d92ecf38dfe58
JamesRileydev <jamesriley.dev@gmail.com>
2020-02-02 17:12:12 -0500

    Added ArtContext

---
+++ ArtMarketplace/Data/Contexts/ArtContext.cs
@@ -0,0 +1,11 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtMarketplace.Data.Contexts
+{
+    public class ArtContext : DbContext
+    {
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
